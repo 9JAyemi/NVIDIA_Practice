16b28ad459abe6018ef81806bf553819212661af
Abiola Bolaji <ab2113@princeton.edu>
2025-01-20 14:52:14 -0500

    d

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -10,9 +10,11 @@ assert {reset |-> (program_counter == 0) && (result == 0)}
 
 # test ADD instruction executes in 1 clock cycle
 assume {registers[rs] > 0 && registers[rt] > 0}
-assert {instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && (reg_write) |-> ##1 registers[$past(rd)] == result }
+assert { alu_op == 2'b00 && (reg_write) |-> ##1 registers[$past(rd)] == result }
 
 # Set the time limit to 1 hour (3600 seconds)
 set_prove_time_limit 3600
 set_engine_mode Tri
 prove -all
+
+# instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000
