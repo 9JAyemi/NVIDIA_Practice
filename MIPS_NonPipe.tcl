b7c73ed388292f5bfc899f6ccd45d53bc02f6ac4
Abiola Bolaji <ab2113@princeton.edu>
2025-01-20 14:24:03 -0500

    hi

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -7,3 +7,8 @@ reset reset
 
 #test that upon reset, resets program counter and alu results
 assert {reset |-> (program_counter == 0) && (result == 0)}
+
+# Set the time limit to 1 hour (3600 seconds)
+set_prove_time_limit 3600
+set_engine_mode Tri
+prove -all
