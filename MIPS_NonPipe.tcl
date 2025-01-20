0e15d9563d24cd97d755dffdf7cfc6d7cc5843e6
Abiola Bolaji <ab2113@princeton.edu>
2025-01-20 14:33:16 -0500

    add test

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -8,6 +8,10 @@ reset reset
 #test that upon reset, resets program counter and alu results
 assert {reset |-> (program_counter == 0) && (result == 0)}
 
+# test ADD instruction executes in 1 clock cycle
+assume {registers[rs] > 0 && registers[rt] > 0}
+assert {instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 result > 0 }
+
 # Set the time limit to 1 hour (3600 seconds)
 set_prove_time_limit 3600
 set_engine_mode Tri
