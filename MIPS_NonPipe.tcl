d08afd88a03d59014a2d9da077b71f632128ce21
Abiola Bolaji <ab2113@princeton.edu>
2025-01-21 13:46:09 -0500

    f

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -11,7 +11,7 @@ assert {reset |-> (pc == 0) && (result == 0)}
 # test ADD instruction executes in 1 clock cycle
  # assume {result > 0}
 # assert { alu_op == 2'b00 && (reg_write) |-> ##1 result == registers[$past(rs)] + registers[$past(rt)]}
-
+assume {pc == 0}
 assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == $past(pc) + 4} 
 assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result) && pc == $past(pc) + 4}
 
