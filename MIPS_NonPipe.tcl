e55ed1f3f072d873a4e8dfc25aa284108c49acf0
Abiola Bolaji <ab2113@princeton.edu>
2025-01-20 23:07:51 -0500

    chagne

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -1,19 +1,19 @@
 analyze -sv MIPS_NonPipe.v
 
-elaborate -top Datapath
+elaborate -top MIPS_CPU
 
 clock clk
 reset reset
 
 #test that upon reset, resets program counter and alu results
-assert {reset |-> (program_counter == 0) && (result == 0)}
+assert {reset |-> (result == 0)}
 
 # test ADD instruction executes in 1 clock cycle
 # assume {result > 0}
 # assert { alu_op == 2'b00 && (reg_write) |-> ##1 result == registers[$past(rs)] + registers[$past(rt)]}
-assume {alu_src == 0}
-assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1  result == (registers[rs]) + (registers[rt])} 
-assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result)}
+# assume {alu_src == 0}
+# assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1  result == (registers[rs]) + (registers[rt])} 
+# assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result)}
 
 
 # Set the time limit to 1 hour (3600 seconds)
