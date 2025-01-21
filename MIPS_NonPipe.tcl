217dbc78af0fb70763f251ae84013c17ed2ed28a
Abiola Bolaji <ab2113@princeton.edu>
2025-01-21 13:37:54 -0500

    d

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -16,7 +16,7 @@ assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instructi
 assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result) && pc == $past(pc) + 4}
 
 assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == pc + 4} 
-assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 registers[(rd)] == (result) && pc == $past(pc) + 4}
+assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 registers[(rd)] == $past(result) && pc == $past(pc) + 4}
 
 
 # Set the time limit to 1 hour (3600 seconds)
