8e70636036e07253eee447ac1898f249986be2a2
Abiola Bolaji <ab2113@princeton.edu>
2025-01-20 22:05:15 -0500

    new add jawn

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -12,7 +12,7 @@ assert {reset |-> (program_counter == 0) && (result == 0)}
 # assume {result > 0}
 # assert { alu_op == 2'b00 && (reg_write) |-> ##1 result == registers[$past(rs)] + registers[$past(rt)]}
 assert {(!reset) && alu_op == 2'b00 && reg_write |-> ##1 (result == $past(registers[rs]) + $past(registers[rt]))} 
-   # |-> ##1 (top.dp.registers[$past(top.dp.rd)] == result)}
+assert {(!reset) && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result)}
 
 # Set the time limit to 1 hour (3600 seconds)
 set_prove_time_limit 3600
