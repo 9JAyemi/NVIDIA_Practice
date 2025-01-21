a675d7ad37ced74e09336802eeadeb958d72fc7c
Abiola Bolaji <ab2113@princeton.edu>
2025-01-20 21:48:50 -0500

    add change

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -6,12 +6,12 @@ clock clk
 reset reset
 
 #test that upon reset, resets program counter and alu results
-assert {reset |-> (program_counter == 0) && (result == 0)}
+assert {reset |-> (top.dp.program_counter == 0) && (result == 0)}
 
 # test ADD instruction executes in 1 clock cycle
 assume {result > 0}
 # assert { alu_op == 2'b00 && (reg_write) |-> ##1 result == registers[$past(rs)] + registers[$past(rt)]}
-assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 (result == $past(top.dp.registers[top.dp.rs]) + $past(top.dp.registers[top.dp.rt])} 
+assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 (result == $past(top.dp.registers[top.dp.rs]) + $past(top.dp.registers[top.dp.rt]))} 
    # |-> ##1 (top.dp.registers[$past(top.dp.rd)] == result)}
 
 # Set the time limit to 1 hour (3600 seconds)
