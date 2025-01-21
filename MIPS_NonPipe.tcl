bcda9b221e0979a8845f84cc397bb3b91b0ec783
Abiola Bolaji <ab2113@princeton.edu>
2025-01-21 13:52:05 -0500

    hi

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -11,12 +11,15 @@ assert {reset |-> (pc == 0) && (result == 0)}
 # test ADD instruction executes in 1 clock cycle
  # assume {result > 0}
 # assert { alu_op == 2'b00 && (reg_write) |-> ##1 result == registers[$past(rs)] + registers[$past(rt)]}
-assume {pc == 4}
-assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == $past(pc) + 4} 
-assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result) && pc == $past(pc) + 4}
 
-assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == pc + 4} 
-assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 registers[$past(rd)] == $past(result) && pc == $past(pc) + 4}
+# assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == $past(pc) + 4} 
+# assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result) && pc == $past(pc) + 4}
+
+assume {pc == 0}
+assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == $past(pc) + 4} 
+assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1  registers[$past(rd)] == $past(result) && pc == $past(pc) + 4}
+assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##0 reg_write == 1};
+
 
 
 # Set the time limit to 1 hour (3600 seconds)
