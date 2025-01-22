d7a40362e52804cc8c88f0bb9bdd8ea128e9bdcd
Abiola Bolaji <ab2113@princeton.edu>
2025-01-22 17:25:23 -0500

    d

--- Intel REU/PUnC.new/PUnC.tcl
+++ Intel REU/PUnC.new/PUnC.tcl
@@ -7,6 +7,7 @@ clock clk
 reset rst
 
 assert {rst |-> ##2 pc_debug_data == 0}
-assert {ir_output[15:12] == 4'b0001 && bit_5 == 0 |-> ##1 pc_debug_data == $past(pc_debug_data) + 1 }
+assume {pc_debug_data == 0}
+assert { && ir_output[15:12] == 4'b0001 && bit_5 == 0 |-> ##1 pc_debug_data == $past(pc_debug_data) + 1 }
 
 prove -all
