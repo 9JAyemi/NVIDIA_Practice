dfe28da1e1321f62814af28f05c77d61dd2621c9
Abiola Bolaji <ab2113@princeton.edu>
2025-01-22 17:34:02 -0500

    d

--- MIPS_NonPipe.tcl
+++ MIPS_NonPipe.tcl
@@ -16,7 +16,7 @@ assert {reset |-> (pc == 0) && (result == 0)}
 # assert {(!reset) && alu_src == 0 && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 && alu_op == 2'b00 && reg_write |-> ##1 (registers[$past(rd)] == result) && pc == $past(pc) + 4}
 
 #assume {pc == 0}
-assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1  result == (registers[rs]) + (registers[rt]) && pc == $past(pc) + 4} 
+assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##2  $past(result) == (registers[$past(rs)]) + (registers[$past(rt)]) && pc == $past(pc) + 4} 
 assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1  registers[$past(rd)] == $past(result) && pc == $past(pc) + 4}
 assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100000 |-> ##1 reg_write == 1};
 assert {(!reset) && instruction[31:26] == 6'b000000 && instruction[5:0] == 6'b100010 |-> ##1  registers[$past(rd)] == $past(result) && pc == $past(pc) + 4}
