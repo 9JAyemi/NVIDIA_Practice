dc8b4440000f72210b9eff4330a181f3ba5553c2
Abiola Bolaji <ab2113@princeton.edu>
2025-01-22 17:14:07 -0500

    d

--- Intel REU/PUnC.new/PUnC.tcl
+++ Intel REU/PUnC.new/PUnC.tcl
@@ -6,7 +6,7 @@ elaborate -top PUnC
 clock clk
 reset rst
 
-assert {rst |-> ##2 pc_ld == 0}
+assert {rst |-> ##2 pc_debug_data == 0}
 assert {ir_output[15:12] == 4'b0001 && bit_5 == 0 |-> ##1 reg_w_en_sig && alu_sig == 2'b00 }
 
 prove -all
