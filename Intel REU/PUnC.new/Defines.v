ddca894194b92f5cf98793e47d9ce1a7f28574de
Abiola Bolaji <ab2113@princeton.edu>
2025-01-22 20:05:55 -0500

    d

--- Intel REU/PUnC.new/Defines.v
+++ Intel REU/PUnC.new/Defines.v
@@ -24,7 +24,6 @@
 `define OC_STR 4'b0111
 `define OC_HLT 4'b1111
 
-
 `define IMM_BIT_NUM 5  // Bit for distinguishing ADDR/ADDI and ANDR/ANDI
 `define IS_IMM 1'b1
 `define JSR_BIT_NUM 11 // Bit for distinguishing JSR/JSRR
@@ -33,3 +32,19 @@
 `define BR_N 11        // Location of special bits in BR instruction
 `define BR_Z 10
 `define BR_P 9
+
+`define mux_select_pc_DATA 2'b10
+`define mux_select_Mem_DATA 2'b01
+`define mux_select_ALU_DATA 2'b00
+`define ALU_inputA_RF0 2'b00
+`define ALU_inputA_pc 2'b01
+`define ALU_inputA_sti 2'b10
+`define ALU_inputB_RF1 1'b0
+`define ALU_inputB_sext 1'b1
+`define ALU_ADD 2'b00
+`define ALU_AND 2'b01
+`define ALU_NOT 2'b10
+`define ALU_PASS 2'b11
+`define RF_pc_addr 3'b111
+`define Mem_mux_pc 1'b0
+`define Mem_mux_ALU 1'b1
--- Intel REU/PUnC.new/PUnC.tcl
+++ Intel REU/PUnC.new/PUnC.tcl
@@ -6,8 +6,8 @@ elaborate -top PUnC
 clock clk
 reset rst
 
-assert {rst |-> ##2 pc_debug_data == 0}
+assert {rst |-> ##1 pc_debug_data == 0}
 
-assert {(!rst) && ir_output[15:12] == 4'b0001 && bit_5 == 0 |-> ##1 pc_debug_data == $past(pc_debug_data)}
+assert {(!rst) && opcode == 4'b0001 && bitfive== 0 |-> ##1 pc_debug_data == $past(pc_debug_data) + 1}
 
 prove -all
