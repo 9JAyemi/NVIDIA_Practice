b1f90dcc29ba3b32aac7a5c16767474eb9c4c19f
Abiola Bolaji <ab2113@princeton.edu>
2025-02-02 16:24:01 -0500

    d

--- Intel REU/sequential_multiplier/MultiplierTester.tcl
+++ Intel REU/sequential_multiplier/MultiplierTester.tcl
@@ -8,8 +8,8 @@ reset rst
 # check if there is a timing attack possible that leaks secret
 assert {multiplicandOne == multiplicandTwo && oneDone -> bothDone}
 
-assume {multiplierOne == multiplicandTwo && multiplierTwo == multiplicandOne}
-assert {productOne == productTwo}
+
+assert {multiplierOne == multiplicandTwo && multiplierTwo == multiplicandOne && bothDone |-> productOne == productTwo}
 
 # Set the time limit to 1 hour (3600 seconds)
 set_prove_time_limit 3600
