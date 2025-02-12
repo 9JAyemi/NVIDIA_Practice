610d1634ed02d295d897f01a95b48de147dc401e
Abiola Bolaji <ab2113@princeton.edu>
2025-02-11 22:07:58 -0500

    d

--- car_park.tcl
+++ car_park.tcl
@@ -7,7 +7,7 @@ reset reset_n
 
 #reset condition
 assert {!reset_n |-> ##1 counter_wait == 0 && green_tmp == 1'b0 && red_tmp == 1'b0}
-assert {!reset_n && current_state == STOP && |-> ##1 next_state == STOP}
+assert {!reset_n && current_state == STOP |-> ##1 next_state == STOP}
 
 set_prove_time_limit 3600
 set_engine_mode Tri
