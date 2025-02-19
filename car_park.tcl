a88e63a8d1ac9eeb08d12f807c1c81a53eb52a6a
Abiola Bolaji <ab2113@princeton.edu>
2025-02-18 21:19:56 -0500

    d

--- car_park.tcl
+++ car_park.tcl
@@ -6,7 +6,7 @@ clock clk
 reset reset_n
 
 #reset condition
-assert {!reset_n |-> ##1 counter_wait == 0 && green_tmp == 1'b0 && red_tmp == 1'b0}
+assert {!reset_n |-> ##1 (counter_wait == 0) && (green_tmp == 1'b0) && (red_tmp == 1'b0)}
 assert {current_state == 3'b001 |=> HEX_1 == 7'b000_0110}
 
 set_prove_time_limit 3600
