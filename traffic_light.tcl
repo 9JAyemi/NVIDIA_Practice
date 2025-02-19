cdacc325d3de7714e4c2df38f6098f89034ecb0f
Abiola Bolaji <ab2113@princeton.edu>
2025-02-18 21:20:27 -0500

    s

--- traffic_light.tcl
+++ traffic_light.tcl
@@ -5,7 +5,7 @@ clock clk
 reset rst_a
 
 #reset condtion
-assert {!rst_a |-> ##1 (state == 32754) && (count == 9)}
+assert {!rst_a |-> ##1 (state == 0) && (count == 0)}
 
 set_prove_time_limit 3600
 set_engine_mode Tri
