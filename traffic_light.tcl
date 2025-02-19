018d96c0f2351a7048d80837fba9a400216770c2
Abiola Bolaji <ab2113@princeton.edu>
2025-02-18 21:43:25 -0500

    d

--- traffic_light.tcl
+++ traffic_light.tcl
@@ -8,7 +8,7 @@ reset rst_a
 assert {(rst_a) |-> ##1 (state == 0) && (count == 0)}
 
 #ensure that state is still in north when count is not at 7
-assert {(state == north && count != 3'b111) |=> (state == $past(state)) }
+assert {(state == north && count < 3'b111) |=> (state == $past(state)) }
 
 #ensure state transition
 assert {(state == north) && (count == 3'b111) |=> (state == north_y)}
